538a9db4ae5349402ab56bed2a615716b40a4616
Prem1singh <shekhawatpremsingh376@gmail.com>
2024-12-12 02:01:18 +0530

    test

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ app.use(cors({
 }));
 app.use('/uploads',express.static('./uploads'));
 app.use(express.json({limit:'8mb'}));
+app.get('/',(req,res)=>{
+    res.send("hello")
+})
 app.use('/auth',AuthRouter);
 app.use('/user',UserRouter);
 app.use('/rooms',RoomRouter);
