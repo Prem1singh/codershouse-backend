7f0bfff9e3bc37644e4c32f4f813fc3747ce869b
Prem1singh <shekhawatpremsingh376@gmail.com>
2024-12-12 11:14:31 +0530

    cors change

--- server.js
+++ server.js
@@ -18,9 +18,6 @@ app.use(cors({
 }));
 app.use('/uploads',express.static('./uploads'));
 app.use(express.json({limit:'8mb'}));
-app.get('/',(req,res)=>{
-    res.send("hello")
-})
 app.use('/auth',AuthRouter);
 app.use('/user',UserRouter);
 app.use('/rooms',RoomRouter);
